460510162337eeb7b455529214acbbe0d6af19aa
fares ahmed <fares.haliim@gmail.com>
2025-02-06 02:53:33 +0200

    start project

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="h1-bold">Next js Mastery </h1>
+      <h2 className="h2-bold">Next js Mastery </h2>
+    </div>
+  );
+}
